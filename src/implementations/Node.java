d1a09c2282deaf8c27c3760d2ba431353861ab46
hernanfuria <hernan.furiasse@hotmail.com>
2023-10-16 13:09:27 -0300

    Node class implemented
    Signed-off-by: hernanfuria <hernan.furiasse@hotmail.com>

--- src/implementations/Node.java
+++ src/implementations/Node.java
@@ -1,8 +1,17 @@
 package implementations;
 
 public class Node {
-	private Object _value;
-	private Node _next_node;
+	private Object _value = null;
+	private Node _next_node = null;
+	
+	public Node(Object value) {
+		setValue(value);
+	}
+	
+	public Node(Object value, Node next_node) {
+		setValue(value);
+		setNext(next_node);
+	}
 	
 	public void setValue(Object value) {
 		/**
@@ -10,6 +19,7 @@ public class Node {
 		 * @param value
 		 *        Any object
 		 */
+		_value = value;
 	}
 	
 	public Object getValue() {
@@ -17,7 +27,7 @@ public class Node {
 		 * Returns the value stored in the Node
 		 * @return value Object
 		 */
-		return null;
+		return _value;
 	}
 	
 	public void setNext(Node next_node) {
@@ -25,12 +35,14 @@ public class Node {
 		 * Sets next Node
 		 * @param next_node
 		 *        Node object*/
+		_next_node = next_node;
 	}
 	
 	public void setNextToNull() {
 		/**
 		 * Sets null as next Node
 		 */
+		_next_node = null;
 	}
 
 	public Object getNext() {
@@ -38,6 +50,7 @@ public class Node {
 		 * Returns next Node
 		 * @returns Node object or null
 		 */
+		return _next_node;
 	}
 	
 	public Object pop() {
@@ -45,7 +58,9 @@ public class Node {
 		 * Returns the value stored in the Node after changing it by null
 		 * @return value Object
 		 */
-		return null;
+		Object aux = getValue();
+		setValue(null);
+		return aux;
 	}
 	
 }
