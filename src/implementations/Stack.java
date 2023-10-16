f06b876be6fb675520dcff6bd5be4030f108f496
hernanfuria <hernan.furiasse@hotmail.com>
2023-10-16 18:05:38 -0300

    Created Stack.java
    New class with methods' declaration and some documentation
    
    Signed-off-by: hernanfuria <hernan.furiasse@hotmail.com>

---
+++ src/implementations/Stack.java
@@ -0,0 +1,24 @@
+package implementations;
+
+public class Stack<T> {
+	private LinkedList<T> values;
+	
+	public void push(T value) {
+		/**
+		 * Inserts a value to the stack
+		 */
+	}
+	
+	public T pop() {
+		/**
+		 * Removes the value at the top of the stack and returns it
+		 */
+	}
+	
+	public boolean isEmpty() {
+		/**
+		 * @return true if the stack is empty, returns false otherwise
+		 */
+	}
+
+}
