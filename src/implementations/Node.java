8c10a7476b83870c63977ab946adf647b0238dfb
hernanfuria <hernan.furiasse@hotmail.com>
2023-10-16 12:57:13 -0300

    Created Node.java
    New class with methods' declaration and some documentation
    
    Signed-off-by: hernanfuria <hernan.furiasse@hotmail.com>

---
+++ src/implementations/Node.java
@@ -0,0 +1,51 @@
+package implementations;
+
+public class Node {
+	private Object _value;
+	private Node _next_node;
+	
+	public void setValue(Object value) {
+		/**
+		 * Sets the value stored in the Node
+		 * @param value
+		 *        Any object
+		 */
+	}
+	
+	public Object getValue() {
+		/**
+		 * Returns the value stored in the Node
+		 * @return value Object
+		 */
+		return null;
+	}
+	
+	public void setNext(Node next_node) {
+		/**
+		 * Sets next Node
+		 * @param next_node
+		 *        Node object*/
+	}
+	
+	public void setNextToNull() {
+		/**
+		 * Sets null as next Node
+		 */
+	}
+
+	public Object getNext() {
+		/**
+		 * Returns next Node
+		 * @returns Node object or null
+		 */
+	}
+	
+	public Object pop() {
+		/**
+		 * Returns the value stored in the Node after changing it by null
+		 * @return value Object
+		 */
+		return null;
+	}
+	
+}
